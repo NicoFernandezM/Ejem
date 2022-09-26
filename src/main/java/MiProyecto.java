7bbce40c32659906c9a2a92695f75df3b896fae7
NicoFernandezM <n.fernandez07@ufromail.cl>
2022-09-26 14:51:06 -0300

    Agrego metodo mostrarMensaje.

--- src/main/java/MiProyecto.java
+++ src/main/java/MiProyecto.java
@@ -1,5 +1,10 @@
 public class MiProyecto {
     public static void main(String[] args) {
         System.out.println("hola");
+        mostrarMensaje();
+    }
+
+    public static void mostrarMensaje() {
+        System.out.println("Mensaje");
     }
 }
